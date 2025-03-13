19e8f1e1e8594145a1f5434fa5994dcac8592a95
Renan G Dutra <r.granatodutra@gmail.com>
2025-03-13 04:25:04 -0300

    feat: update prisma schema to mysql

--- prisma/migrations/20250311154207_update_files/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `dir_type` on the `files` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "files" DROP COLUMN "dir_type",
-ADD COLUMN     "dir_type" "FileDirType" NOT NULL;
--- prisma/migrations/20250312121016_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "storages" ADD COLUMN     "isDefault" BOOLEAN NOT NULL DEFAULT false;
